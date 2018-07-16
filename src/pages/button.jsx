38c682f5d9d00d3b97f5ef4f271653d19f895252
[thecubix] <[timon.forrer@gmail.com]>
2018-07-16 23:22:27 +0200

    Testpage for button component

---
+++ src/pages/button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Heading from '../components/Heading';
+import ButtonLink from '../components/ButtonLink';
+
+const IndexPage = () => (
+  <div style={{
+    marginTop: '100px'
+  }}>
+    <ButtonLink dest="/" className="z-1">Home</ButtonLink>
+  </div>
+)
+
+export default IndexPage
