f25b849a589939845e13e2bad4ff4c8a5517278e
[thecubix] <[timon.forrer@gmail.com]>
2018-07-16 23:21:59 +0200

    Styled the Button

--- src/components/ButtonLink/buttonLink.jsx
+++ src/components/ButtonLink/buttonLink.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'gatsby-link';
+//import Arrow from './arrow-thin-right.svg';
+
+import Arrow from '-!babel!svg-react-loader?name=Arrow!./arrow-long-thin-right.svg';
 
 const ButtonLink = ({ children, dest, className }) => (
-  <Link to={dest} className={className}>{children}</Link>
+  <Link to={dest} className={`link btn-link ${className}`}>
+    <span className="btn-link__text">{children}</span> <Arrow className="btn-link__arrow" />
+    <div className="btn-link__backdrop"></div>
+  </Link>
 );
 
 export default ButtonLink;
