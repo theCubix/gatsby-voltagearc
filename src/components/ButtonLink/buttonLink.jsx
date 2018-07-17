312b383d40a3f42c668099b296446110931fcf8f
[thecubix] <[timon.forrer@gmail.com]>
2018-07-17 23:11:13 +0200

    Updated icon name to fit namingconvention

--- src/components/ButtonLink/buttonLink.jsx
+++ src/components/ButtonLink/buttonLink.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import Link from 'gatsby-link';
-//import Arrow from './arrow-thin-right.svg';
 
-import Arrow from '-!babel!svg-react-loader?name=Arrow!./arrow-long-thin-right.svg';
+import ArrowIcon from '-!babel!svg-react-loader?name=Arrow!./arrow-long-thin-right.svg';
 
 const ButtonLink = ({ children, dest, className }) => (
   <Link to={dest} className={`link btn-link ${className}`}>
-    <span className="btn-link__text">{children}</span> <Arrow className="btn-link__arrow" />
+    <span className="btn-link__text">{children}</span> <ArrowIcon className="btn-link__arrow" />
     <div className="btn-link__backdrop"></div>
   </Link>
 );
